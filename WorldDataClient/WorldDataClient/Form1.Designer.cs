2a02c1e68ffcdbd492f13ba05735e837ba4bcf7b
chauthuanduc <135441664+chauthuanduc@users.noreply.github.com>
2025-02-05 14:17:15 +0700

    Add files via upload

---
+++ WorldDataClient/WorldDataClient/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WorldDataClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbFunctions = new System.Windows.Forms.ComboBox();
+            this.txtParameter = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.lblPopulation = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbFunctions
+            // 
+            this.cmbFunctions.FormattingEnabled = true;
+            this.cmbFunctions.Location = new System.Drawing.Point(76, 65);
+            this.cmbFunctions.Name = "cmbFunctions";
+            this.cmbFunctions.Size = new System.Drawing.Size(117, 24);
+            this.cmbFunctions.TabIndex = 0;
+            // 
+            // txtParameter
+            // 
+            this.txtParameter.Location = new System.Drawing.Point(257, 66);
+            this.txtParameter.Name = "txtParameter";
+            this.txtParameter.Size = new System.Drawing.Size(100, 22);
+            this.txtParameter.TabIndex = 1;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(434, 65);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(76, 147);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(691, 199);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // lblPopulation
+            // 
+            this.lblPopulation.AutoSize = true;
+            this.lblPopulation.Location = new System.Drawing.Point(73, 380);
+            this.lblPopulation.Name = "lblPopulation";
+            this.lblPopulation.Size = new System.Drawing.Size(44, 16);
+            this.lblPopulation.TabIndex = 4;
+            this.lblPopulation.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPopulation);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtParameter);
+            this.Controls.Add(this.cmbFunctions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbFunctions;
+        private System.Windows.Forms.TextBox txtParameter;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label lblPopulation;
+    }
+}
+
---
+++ WorldDataClient/WorldDataClient/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldDataClient.ServiceReference1;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WorldDataClient
+{
+    public partial class Form1 : Form
+    {
+        private WorldServiceSoapClient client; // Khai báo client toàn cục
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new WorldServiceSoapClient();
+            cmbFunctions.Items.AddRange(new string[]
+            {
+                "GetAllCountries",
+                "GetCountryByCode",
+                "GetCityByName",
+                "GetCitiesByCountryCode",
+                "GetCountriesByRegion",
+                "GetPopulationByCountry"
+            });
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string selectedFunction = cmbFunctions.SelectedItem?.ToString();
+            string parameter = txtParameter.Text;
+
+            try
+            {
+                switch (selectedFunction)
+                {
+                    case "GetAllCountries":
+                        dataGridView.DataSource = client.GetAllCountries();
+                        break;
+
+                    case "GetCountryByCode":
+                        if (!string.IsNullOrEmpty(parameter))
+                            dataGridView.DataSource = client.GetCountryByCode(parameter);
+                        else
+                            MessageBox.Show("Please enter a country code.");
+                        break;
+
+                    case "GetCityByName":
+                        if (!string.IsNullOrEmpty(parameter))
+                            dataGridView.DataSource = client.GetCityByName(parameter);
+                        else
+                            MessageBox.Show("Please enter a city name.");
+                        break;
+
+                    case "GetCitiesByCountryCode":
+                        if (!string.IsNullOrEmpty(parameter))
+                            dataGridView.DataSource = client.GetCitiesByCountryCode(parameter);
+                        else
+                            MessageBox.Show("Please enter a country code.");
+                        break;
+
+                    case "GetCountriesByRegion":
+                        if (!string.IsNullOrEmpty(parameter))
+                            dataGridView.DataSource = client.GetCountriesByRegion(parameter);
+                        else
+                            MessageBox.Show("Please enter a region.");
+                        break;
+
+                    case "GetPopulationByCountry":
+                        if (!string.IsNullOrEmpty(parameter))
+                        {
+                            int population = client.GetPopulationByCountry(parameter);
+                            lblPopulation.Text = $"Population: {population}";
+                        }
+                        else
+                            MessageBox.Show("Please enter a country code.");
+                        break;
+
+                    default:
+                        MessageBox.Show("Please select a function.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
